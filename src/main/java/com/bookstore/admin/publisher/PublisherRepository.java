3754df45b70e48bd4dcdaa283baaa1e6ae6aebc9
WillLe2502 <quangnhatminhle25021998@gmail.com>
2023-01-03 15:37:14 +1100

    updateUser

--- src/main/java/com/bookstore/admin/publisher/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.admin.publisher;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bookstore.admin.entity.Publisher;
-
-public interface  PublisherRepository extends CrudRepository<Publisher, Integer>{
-
-}
